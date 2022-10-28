64513e70e312118e4e7421b6235330a5611b339c
yosra mekaoui <yosra.mekaoui@esprit.tn>
2022-10-28 12:02:46 +0200

    update

--- src/app/provider/provider-list/provider-list.component.ts
+++ src/app/provider/provider-list/provider-list.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-provider-list',
   templateUrl: './provider-list.component.html',
   styleUrls: ['./provider-list.component.css']
+  
 })
 export class ProviderListComponent implements OnInit {
 
